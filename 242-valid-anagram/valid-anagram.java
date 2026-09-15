017f015ddafdc5491de7876a4ee35841d4b34721
MONIKA B <monika.balakumar18@gmail.com>
2026-09-15 20:55:04 +0530

    Time: 18 ms (13.41%) | Memory: 46.8 MB (9.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,34 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
+        if(s.length()!=t.length())
+        {
             return false;
         }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-      
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-
-            if (!map.containsKey(ch)) {
+        HashMap<Character,Integer>hm=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+        }for(int i=0;i<t.length();i++)
+        {
+            char ch=t.charAt(i);
+            if(!hm.containsKey(ch))
+            {
                 return false;
             }
-
-            map.put(ch, map.get(ch) - 1);
-
-            if (map.get(ch) < 0) {
+           
+            hm.put(ch,hm.get(ch)-1);
+            if(hm.get(ch)<0)
+            {
                 return false;
+
             }
         }
-
         return true;
+
     }
 }
-
