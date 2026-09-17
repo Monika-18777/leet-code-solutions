89979a1c03ed40cbfa4e7461e1a010bbd0e745c1
MONIKA B <monika.balakumar18@gmail.com>
2026-09-17 20:48:09 +0530

    Time: 5 ms (100.00%) | Memory: 46.9 MB (49.43%) - LeetSync

---
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSemiRepetitiveSubstring(String s) {
+        int left = 0;
+        int pairs = 0;
+        int ans = 1;
+
+        for (int right = 1; right < s.length(); right++) {
+
+            if (s.charAt(right) == s.charAt(right - 1)) {
+                pairs++;
+            }
+
+            while (pairs > 1) {
+                if (s.charAt(left) == s.charAt(left + 1)) {
+                    pairs--;
+                }
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
